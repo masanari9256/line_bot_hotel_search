8309017b6bcf91acc0435e7b79e2c6f803ce71c4
cloud9_masanari9256 <masanari92562@gmail.com>
2021-06-29 10:54:44 +0000

    [add]オウム返し機能をデプロイ

---
+++ app/controllers/line_bot_controller.rb
@@ -0,0 +1,21 @@
+class LineBotController < ApplicationController
+  protect_from_forgery except: [:callback]
+  
+  def callback
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+     unless client.validate_signature(body, signature)
+       return head :bad_request
+     end
+  end
+  
+  private
+ 
+     def client
+       @client ||= Line::Bot::Client.new { |config|
+         config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+         config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+       }
+     end
+     
+end
